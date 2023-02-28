6fdf60cdb2087cb4e16a39fe18299e3e1dd683c2
ben <benrobertson9876@gmail.com>
2023-02-28 19:20:27 +1100

    set array

--- functions/trusted_fact_names.pp
+++ functions/trusted_fact_names.pp
@@ -1,6 +1,6 @@
 # See https://puppet.com/docs/puppet/latest/lang_write_functions_in_puppet.html
 # for more information on native puppet functions.
-function update_trusted_facts::trusted_fact_names() >> Hash {
+function update_trusted_facts::trusted_fact_names() >> Array {
   $array = ['pp_role','pp_uuid','pp_environment','pp_apptier','pp_department','pp_datacenter']
   # $hash = { 'pp_role' => '','pp_uuid' => '','pp_environment' => '','pp_apptier' => '','pp_department' => '','pp_datacenter' => '' }
 }
