7820c88a95eacd0a7caecb7da3414ea72586afe9
ben <benrobertson9876@gmail.com>
2023-03-01 17:43:56 +1100

    with set csr attributes

--- tasks/retrieve_trusted_facts.rb
+++ tasks/retrieve_trusted_facts.rb
@@ -1,15 +1,10 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 # frozen_string_literal: true
 
-# Task whichs lookups existing csr_attributes on a system. And merges the existing trusted facts 
-#
-#
-
 require 'open3'
 require 'puppet'
 require 'openssl'
 require 'yaml'
-
 
 def get_cert_location
   if Gem.win_platform?
@@ -44,59 +39,7 @@ trusted_facts_oid = { 'pp_uuid' => '1.3.6.1.4.1.34380.1.1.1', 'pp_instance_id' =
 
 cert_location = get_cert_location
 
-existing_facts = read_trusted_facts(cert_location, trusted_facts_oid)
+trusted_facts = read_trusted_facts(cert_location, trusted_facts_oid)
 
-def csr_attribute_location
-  if Gem.win_platform?
-    'C:/ProgramData/PuppetLabs/puppet/etc/csr_attributes.yaml'
-  else
-    '/etc/puppetlabs/puppet/csr_attributes.yaml'
-  end
-end
+puts "Trusted facts are #{trusted_facts}"
 
-def get_existing_csr(csr_attr_file_location)
-  if File.exists?(csr_attr_file_location)
-    data = YAML.safe_load(File.read(csr_attr_file_location))
-  else
-    nil
-  end
-end
-
-def merge_facts(existing_csr, new_trusted_facts)
-  if existing_csr.keys.include?('extension_requests')
-    combined_facts = existing_csr['extension_requests'].merge(new_trusted_facts)
-    existing_csr['extension_requests'] = combined_facts
-    existing_csr
-  else
-    existing_csr['extension_requests'] = {}
-    combined_facts = existing_csr['extension_requests'].merge(new_trusted_facts)
-    existing_csr['extension_requests'] = combined_facts
-    existing_csr
-  end
-end
-
-# Get csr file location
-csr_attr_file_location = csr_attribute_location
-
-# Load existing rules
-existing_csr = get_existing_csr(csr_attr_file_location)
-
-# set existing facts, we don't want to keep the facts from the csr_attributes file
-existing_csr['extension_requests'] = existing_facts
-
-# new_trusted_facts = params['trusted_facts']
-new_trusted_facts = {'pp_role' => 'doge', 'pp_environment' => 'dog', 'pp_department' => 'blah', 'pp_datacenter' => 'louie'}
-
-puts "Existing facts are #{existing_csr}"
-puts "New facts are #{new_trusted_facts}"
-
-# Merge the hash
-if existing_csr == nil
-  merged_csr = { 'extension_requests' => new_trusted_facts }
-else
-  merged_csr = merge_facts(existing_csr, new_trusted_facts)
-end
-
-puts "Combined hash is #{merged_csr}"
-
-File.write(csr_attr_file_location, merged_csr.to_yaml)
