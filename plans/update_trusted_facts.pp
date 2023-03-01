1834346655f03e51791a7ee42f0042adfc1a2ec6
ben <benrobertson9876@gmail.com>
2023-03-02 10:23:44 +1100

    add primary server

--- plans/update_trusted_facts.pp
+++ plans/update_trusted_facts.pp
@@ -89,7 +89,7 @@ plan update_trusted_facts::update_trusted_facts (
     out::message("Nodes to regen certs on ${nodes_to_regen_cert}")
     if $nodes_to_regen_cert != undef {
       if $noop != true {
-        run_command("puppet infrastructure run regenerate_agent_certificate agent=${nodes_to_regen_cert}")
+        run_command("puppet infrastructure run regenerate_agent_certificate agent=${nodes_to_regen_cert}", $pe_primary_server)
       }
     }
   }
