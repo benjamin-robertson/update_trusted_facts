d93d930016367c08daaae7ec8a7f0a7c057e6bbc
ben <benrobertson9876@gmail.com>
2023-03-02 13:43:03 +1100

    Print results

--- plans/update_trusted_facts.pp
+++ plans/update_trusted_facts.pp
@@ -73,6 +73,7 @@ plan update_trusted_facts::update_trusted_facts (
                                                   'pe_primary_server'         => $pe_server_target.name,
                                                   'ignore_infra_status_error' => $ignore_infra_status_error,
                                                   '_catch_errors'             => true )
+    out::message("Results are ${confirm_pe_primary_server_results.ok_set}")
     if $confirm_pe_primary_server_results.ok_set == undef {
       fail_plan("Primary server provided not the primary server for this Puppet Enterprise installation: ${pe_server_target.name} ")
     }
